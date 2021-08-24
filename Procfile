91911cb17a7cab8c5e33bd3c959c1a1cd42fa096
4mbk0r <4mbk0r@gmail.com>
2021-08-23 20:17:46 -0400

    cambio name

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn Proyecto1.wsgi
+web: gunicorn Projecto1.wsgi
--- Projecto1/settings.py
+++ Projecto1/settings.py
@@ -142,3 +142,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import dj_database_url  
 db_from_env = dj_database_url.config(conn_max_age=500)  
 DATABASES['default'].update(db_from_env)
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATIC_URL = '/static/'# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (  
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
